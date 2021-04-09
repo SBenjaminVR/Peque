62c36d7268c46c63968b619b57808d652f0c84b4
Benjamin Valdez <benjamin.vr.1998@gmail.com>
2021-04-08 23:01:18 -0500

    Added template for tokens

--- pequeScanner.py
+++ pequeScanner.py
@@ -2,3 +2,42 @@ import ply.lex as lex           # Scanner
 import ply.yacc as yacc         # Parser
 from pathlib import Path        # Read files
 import sys
+
+reserved = {
+    'program': 'PROGRAM',
+    'print': 'PRINT',
+    'var': 'VAR',
+    'int': 'INT',
+    'float': 'FLOAT',
+    'if': 'IF',
+    'else': 'ELSE'
+}
+
+tokens = [
+    'ID', 'STRING',
+    'CTEI', 'CTEF', 'CTESTRING',
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+    'DOTS', 'EQUALS', 'SEMICOLON', 'PERIOD',
+    'LESS', 'BIGGER', 'DIFFERENT',
+    'L_PARENTHESIS', 'R_PARENTHESIS', 'L_BRACKET', 'R_BRACKET',
+] + list(reserved.values())
+
+# Expresiones regulares para tokens simples
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_TIMES = r'\*'
+t_DIVIDE = r'\/'
+t_DOTS = r'\:'
+t_EQUALS = r'\='
+t_SEMICOLON = r'\;'
+t_PERIOD = r'\.'
+t_LESS = r'\<'
+t_BIGGER = r'\>'
+t_DIFFERENT = r'\<\>'
+t_L_PARENTHESIS = r'\('
+t_R_PARENTHESIS = r'\)'
+t_L_BRACKET = r'\{'
+t_R_BRACKET = r'\}'
+
+# Caracteres ignorados (Espacios y tabs)
+t_ignore = ' \t'
