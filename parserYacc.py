742f71129001e69a9753e121fbfa42a70ba99b4d
JuanCGar <juangarza373@gmail.com>
2021-05-28 23:50:39 -0500

    For terminado

--- parserYacc.py
+++ parserYacc.py
@@ -161,14 +161,22 @@ def p_for_inicio(p):
     '''
     for_inicio : empty
     '''
-    
     Saltos.append(cont)
     p[0]= None
 def p_for_temp(p):
     '''
     for_temp : empty
     '''
-    TemporalesFor.push(values.pop())
+    iz = values.pop()
+    GenerarNuevoTemporal('int')
+    values.push(Temporales[-1])
+    res = values.top()
+    TemporalesFor.push(res)
+    CrearCuadruplo('=', iz, '_', res)
+    
+   
+
+    
     
     
     p[0]= None
@@ -176,12 +184,15 @@ def p_for_revision(p):
     '''
     for_revision : empty
     '''
-    GenerarNuevoTemporal()
+    
     #chechar por las variables y expresiones
+    print('stack')
+    values.printStack()
     popper.push('>=')
     GenerarCuadruploDeOperador(popper,values,tipos)
     Saltos.append(cont)
-    CrearCuadruplo('GotoF',Temporales[-1],'_','_')
+    CrearCuadruplo('GotoF',values.pop(),'_','_')
+   
 
     p[0]= None
 
@@ -189,15 +200,21 @@ def p_for_suma (p):
     '''
     for_suma : empty
     '''
-    popper.push('+')
-    values.push(TemporalesFor.pop())
-    #PENDIENTE REVISION DE TIPOS
-    GenerarCuadruploDeOperador(popper,values,tipos)
+   
     p[0]=None
 def p_for_final(p):
     '''
     for_final : empty
     '''
+
+    print('generar for')
+    
+    
+    variableFor = TemporalesFor.pop()
+    tipo = tipos.pop()
+    #falta comprobar tipos del for
+    CrearCuadruplo('+',values.pop(),variableFor,variableFor)
+    
     global cont
     falseJump = Saltos[-1]
     Saltos.pop()
@@ -205,6 +222,8 @@ def p_for_final(p):
     Saltos.pop()
     CrearCuadruplo('Goto','_','_',Ret)
     Fill(falseJump,cont)
+
+    
     p[0] = None
 #--------------------------While--------------------
 def p_repeticion_condicional(p):
